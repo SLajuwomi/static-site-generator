054e5417b0ddd5c5d0ce830ced1c5faf4de5657a
SLajuwomi <stephenlajuwomi@gmail.com>
2025-08-20 00:34:26 -0500

    got split delimiter working and tested

---
+++ src/split_nodes.py
@@ -0,0 +1,25 @@
+from textnode import TextNode, TextType
+
+
+def split_nodes_delimiter(old_nodes, delimiter, text_type):
+    new_nodes = []
+    for node in old_nodes:
+        if node.text_type is not TextType.PLAIN:
+            new_nodes.append(node)
+        split_text_list = node.text.split(delimiter)
+        if len(split_text_list) % 2 == 0:
+            raise Exception("Invalid Markdown syntax: missing closing delimiter")
+        for index, string in enumerate(split_text_list):
+            if index % 2 == 0:
+                new_nodes.append(TextNode(string, TextType.PLAIN))
+            elif string == "":
+                pass
+            else:
+                new_nodes.append(TextNode(string, text_type))
+
+    print(new_nodes)
+    return new_nodes
+
+
+node = TextNode("This is a text with a `code block` word", TextType.PLAIN)
+split_nodes_delimiter([node], "`", TextType.CODE)
---
+++ src/test_split_nodes.py
@@ -0,0 +1,60 @@
+import unittest
+
+from split_nodes import split_nodes_delimiter
+from textnode import TextNode, TextType
+
+
+class TestSplitNodes(unittest.TestCase):
+    def test_split_nodes(self):
+        code_node = TextNode("This is a text with a `code block` word", TextType.PLAIN)
+        italic_node = TextNode("This is _an italic word_.", TextType.PLAIN)
+        italic_nested_bold_node = TextNode(
+            "This is an _italic and **bold** word_.", TextType.PLAIN
+        )
+        bold_node = TextNode("This is a **bold** node", TextType.PLAIN)
+
+        self.assertEqual(
+            split_nodes_delimiter(
+                old_nodes=[code_node], delimiter="`", text_type=TextType.CODE
+            ),
+            [
+                TextNode("This is a text with a ", TextType.PLAIN),
+                TextNode("code block", TextType.CODE),
+                TextNode(" word", TextType.PLAIN),
+            ],
+        )
+
+        self.assertEqual(
+            split_nodes_delimiter(
+                old_nodes=[italic_node], delimiter="_", text_type=TextType.ITALIC
+            ),
+            [
+                TextNode("This is ", TextType.PLAIN),
+                TextNode("an italic word", TextType.ITALIC),
+                TextNode(".", TextType.PLAIN),
+            ],
+        )
+
+        self.assertEqual(
+            split_nodes_delimiter(
+                old_nodes=[italic_nested_bold_node],
+                delimiter="_",
+                text_type=TextType.ITALIC,
+            ),
+            [
+                TextNode("This is an ", TextType.PLAIN),
+                TextNode("italic and **bold** word", TextType.ITALIC),
+                TextNode(".", TextType.PLAIN),
+            ],
+        )
+
+        self.assertEqual(
+            split_nodes_delimiter(
+                old_nodes=[bold_node], delimiter="**", text_type=TextType.BOLD
+            ),
+            [
+                TextNode("This is a ", TextType.PLAIN),
+                TextNode("bold", TextType.BOLD),
+                TextNode(" node", TextType.PLAIN),
+            ],
+        )
