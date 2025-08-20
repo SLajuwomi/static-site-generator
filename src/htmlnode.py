c6839c1e0e46f38e5e9dbf1172e3706378d52e2d
SLajuwomi <stephenlajuwomi@gmail.com>
2025-08-19 19:10:41 -0500

    fix bug with spaces in html node

--- src/htmlnode.py
+++ src/htmlnode.py
@@ -8,11 +8,16 @@ class HTMLNode:
     def to_html(self):
         raise NotImplementedError
 
+    # def props_to_html(self):
+    #    final_string = ""
+    #    for key, value in self.props.items():
+    #        final_string += f'{key}="{value}"'
+    #    return final_string
+
     def props_to_html(self):
-        final_string = ""
-        for key, value in self.props.items():
-            final_string += f'{key}="{value}"'
-        return final_string
+        if not self.props:
+            return ""
+        return " ".join(f'{key}="{value}"' for key, value in self.props.items())
 
     def __repr__(self):
         return f"HTMLNode {self.tag, self.value, self.children, self.props}"
