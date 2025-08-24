a283bfeeac0d206ab446bbc0cea5f6b8053313da
SLajuwomi <stephenlajuwomi@gmail.com>
2025-08-24 17:26:39 -0500

    block to block type implemented

--- src/test_extract_markdown.py
+++ src/test_extract_markdown.py
@@ -5,6 +5,7 @@ from extract_markdown import (
     extract_markdown_links,
     markdown_to_blocks,
 )
+from blocktype import block_to_block_type, BlockType
 
 
 class TestExtractMarkdown(unittest.TestCase):
@@ -59,3 +60,17 @@ Testing double new line.
                 "Testing double new line.",
             ],
         )
+
+    def test_block_to_block_types(self):
+        block = "# heading"
+        self.assertEqual(block_to_block_type(block), BlockType.HEADING)
+        block = "```\ncode\n```"
+        self.assertEqual(block_to_block_type(block), BlockType.CODE)
+        block = "> quote\n> more quote"
+        self.assertEqual(block_to_block_type(block), BlockType.QUOTE)
+        block = "- list\n- items"
+        self.assertEqual(block_to_block_type(block), BlockType.ULIST)
+        block = "1. list\n2. items"
+        self.assertEqual(block_to_block_type(block), BlockType.OLIST)
+        block = "paragraph"
+        self.assertEqual(block_to_block_type(block), BlockType.PARAGRAPH)
