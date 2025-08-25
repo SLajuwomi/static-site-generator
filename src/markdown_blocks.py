a85b217cbb583f2b1f3bd846595048371e6487e9
SLajuwomi <stephenlajuwomi@gmail.com>
2025-08-25 17:03:43 -0500

    code node is now not doing inline parsing

--- src/markdown_blocks.py
+++ src/markdown_blocks.py
@@ -1,5 +1,6 @@
 from enum import Enum
 from htmlnode import HTMLNode, LeafNode, ParentNode
+from textnode import TextNode, TextType
 
 
 class BlockType(Enum):
@@ -53,22 +54,24 @@ def markdown_to_html_node(markdown):
         print(f"Block Type: {type_of_block}")
         print(f"List Items: {list_items}")
         if type_of_block == BlockType.QUOTE:
-            new_html_node = HTMLNode("blockquote", block)
+            new_html_node = LeafNode("blockquote", block)
         if type_of_block == BlockType.HEADING:
             for char in block:
                 if char == "#":
                     heading_count += 1
                 else:
                     break
-            new_html_node = HTMLNode(f"h{heading_count}", block)
+            new_html_node = ParentNode(f"h{heading_count}", block)
         if type_of_block == BlockType.PARAGRAPH:
             new_html_node = LeafNode("p", block)
             print(new_html_node.to_html())
         if type_of_block == BlockType.CODE:
             code_content = "\n".join(list_items[1:-1])
-            code_node = LeafNode("code", code_content)
-            pre_node = ParentNode("pre", children=[code_node])
-            # print(pre_node.to_html())
+            text_code_node = TextNode(code_content, TextType.TEXT)
+            text_code_html_node = text_code_node.text_node_to_html_node()
+            # code_node = LeafNode("code", code_content)
+            pre_node = ParentNode("pre", children=[text_code_html_node])
+            print(pre_node.to_html())
         if type_of_block == BlockType.ULIST:
             li_nodes = [
                 LeafNode("li", item.lstrip("- ").strip()) for item in list_items
@@ -83,8 +86,10 @@ def markdown_to_html_node(markdown):
 # - Item 3
 # """
 markdown = """
-This is some random paragraph text
-some more random text
+```
+This is a code node that _italic_ should not change **bolod**
+testing above
+```
 """
 nodes = markdown_to_html_node(markdown)
 print(nodes)
