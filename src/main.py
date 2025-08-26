972ec07bf3d4f843c21e00bff864fcac90e773a3
SLajuwomi <stephenlajuwomi@gmail.com>
2025-08-26 12:59:50 -0500

    wrote recursive function to copy all items from a src dir to a dst dir

--- src/main.py
+++ src/main.py
@@ -1,12 +1,27 @@
+import os
+import shutil
 from textnode import TextNode
 from textnode import TextType
 
 
 def main():
-    new_text_node = TextNode(
-        "This is some anchor text", TextType.LINK, "https://boot.dev"
-    )
-    print(new_text_node)
+    src_to_dst("../static", "../public")
+
+
+def src_to_dst(src, dst):
+    shutil.rmtree(dst)
+    os.mkdir(dst)
+    # print(os.listdir(src))
+    # print(os.listdir(dst))
+    for item_name in os.listdir(src):
+        item_path = os.path.join(src, item_name)
+        print(f"Current item path: {item_path}")
+        if os.path.isfile(item_path):
+            shutil.copy(item_path, dst=dst)
+        elif os.path.isdir(item_path):
+            os.mkdir(f"{dst}/{item_name}")
+            src_to_dst(item_path, dst=f"{dst}/{item_name}")
+    print(f"At end {os.listdir(dst)}")
 
 
 if __name__ == "__main__":
