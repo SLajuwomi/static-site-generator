d8a6ed99a68865b05da447f7438a55f7c06876ec
SLajuwomi <stephenlajuwomi@gmail.com>
2025-08-26 13:03:58 -0500

    edit ignore file

--- src/main.py
+++ src/main.py
@@ -11,17 +11,13 @@ def main():
 def src_to_dst(src, dst):
     shutil.rmtree(dst)
     os.mkdir(dst)
-    # print(os.listdir(src))
-    # print(os.listdir(dst))
     for item_name in os.listdir(src):
         item_path = os.path.join(src, item_name)
-        print(f"Current item path: {item_path}")
         if os.path.isfile(item_path):
             shutil.copy(item_path, dst=dst)
         elif os.path.isdir(item_path):
             os.mkdir(f"{dst}/{item_name}")
             src_to_dst(item_path, dst=f"{dst}/{item_name}")
-    print(f"At end {os.listdir(dst)}")
 
 
 if __name__ == "__main__":
