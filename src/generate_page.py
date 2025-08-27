86d3bde842b4dc82e71c9696ed9394d44ab8abf4
SLajuwomi <stephenlajuwomi@gmail.com>
2025-08-27 12:33:09 -0500

    removed unnecessary code

--- src/generate_page.py
+++ src/generate_page.py
@@ -18,7 +18,8 @@ def generate_page(from_path, dest_path, template_path, basepath):
     replace_href = with_content.replace('href="/', f'href="{basepath}')
     replace_src = replace_href.replace('src="/', f'src="{basepath}')
     dest_dir = os.path.dirname(dest_path)
-    os.makedirs(dest_dir, exist_ok=True)
+    if dest_dir != "":
+        os.makedirs(dest_dir, exist_ok=True)
     with open(dest_path, "w") as f:
         f.write(replace_src)
 
@@ -30,15 +31,6 @@ def generate_pages_recursive(dir_path_content, template_path, dest_dir_path, bas
             rel_file_path = os.path.relpath(full_file_path, dir_path_content)
             html_rel_file_path = rel_file_path.replace("md", "html")
             complete_dest_dir_path = os.path.join(dest_dir_path, html_rel_file_path)
-            parts = complete_dest_dir_path.split(os.sep)
-            current_path = ""
-            for part in parts:
-                if not part:
-                    continue
-                current_path = os.path.join(current_path, part)
-                path_obj = Path(current_path)
-                if not os.path.isdir(current_path) and not bool(path_obj.suffix):
-                    os.makedirs(current_path, exist_ok=True)
             if full_file_path.endswith("md"):
                 generate_page(
                     full_file_path, complete_dest_dir_path, template_path, basepath
